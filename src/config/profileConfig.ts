ec07202a2c8fff4ae20102e5f6e81b8ce9c7e048
Parssama2233 <hello.Pars@outlook.com>
2025-11-02 12:08:16 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -15,5 +15,10 @@ export const profileConfig: ProfileConfig = {
       icon: "fa6-brands:github",
       url: "https://github.com/Parssama2233",
     },
+    {
+      name: "X",
+      icon: "fa6-brands:X",
+      url: "https://x.com/SLKaFAGNHI11131",
+    },
   ],
 };
