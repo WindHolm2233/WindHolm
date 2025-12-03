70b0d4555876ea471d4fe41937bf4eb84168f890
Parssama2233 <hello.windholm@gmail.com>
2025-12-03 13:15:55 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -86,9 +86,9 @@ export const siteConfig: SiteConfig = {
     // 背景图片配置
     src: {
       // 桌面背景图片
-      desktop: "/assets/images/meme.png",
+      desktop: "/assets/images/mememe.gif",
       // 移动背景图片
-      mobile: "/assets/images/pe.jpg",
+      mobile: "/assets/images/mememe.gif",
     },
 
     // 图片位置
