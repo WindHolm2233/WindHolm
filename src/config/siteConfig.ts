fbe24cb90f15779f25d6b248273abc7477a46e2a
WindHolm2233 <hello.windholm@gmail.com>
2026-02-10 16:38:28 +0800

    Update siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,7 +7,7 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
   title: "WindHolm",
-  subtitle: "海风与你",
+  subtitle: "海风屿你",
   description:
     "WindHolm 是一款基于 Astro 框架开发的清新美观且现代化个人博客主题，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
   keywords: [
